6b88606a21b97c6beecab1fc0a4571a9dde4e2e2
Shah097 <manveershah007@gmail.com>
2025-09-19 04:37:40 -0700

    bugs

--- src/app/helper/services/postcodeApi.js
+++ src/app/helper/services/postcodeApi.js
@@ -1,5 +1,5 @@
 export async function createPostcode(code) {
-  const res = await fetch("/api/postcodes", {
+  const res = await fetch("/api/form/postcode", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ postcode: code }),
@@ -8,12 +8,12 @@ export async function createPostcode(code) {
 }
 
 export async function getPostcodes() {
-  const res = await fetch("/api/postcodes");
+  const res = await fetch("/api/form/postcode");
   return res.json();
 }
 
 export async function updatePostcode(id, code) {
-  const res = await fetch(`/api/postcodes/${id}`, {
+  const res = await fetch(`/api/form/postcode/${id}`, {
     method: "PUT",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ postcode: code }),
@@ -22,7 +22,7 @@ export async function updatePostcode(id, code) {
 }
 
 export async function deletePostcode(id) {
-  const res = await fetch(`/api/postcodes/${id}`, {
+  const res = await fetch(`/api/form/postcode/${id}`, {
     method: "DELETE",
   });
   return res.json();
