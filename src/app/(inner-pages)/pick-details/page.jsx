7cfcd58efd194b9230e652d3328d090731b662aa
Shah097 <manveershah007@gmail.com>
2025-09-19 04:36:33 -0700

    update

--- src/app/(inner-pages)/pick-details/page.jsx
+++ src/app/(inner-pages)/pick-details/page.jsx
@@ -1,7 +1,7 @@
 import Form from '@/components/form/mult/MultiStepForm';
 import Animations from "@/components/animations/Animations";
 
-export default function MultiStepForm() {
+export default function PickDetailsPage() {
     return(
         <div>
          <Form />
